83c87660485889b3f7b1f66a52140877f00c3e20
rixiobarrios <rixiobarrios.consulado@gmail.com>
2023-08-23 08:21:25 -0500

    finished assesment

--- script.js
+++ script.js
@@ -339,3 +339,229 @@
 // console.log(ArrayChallenge([3, 5, -1, 8, 12])); //=> true
 
 //------------------------------------------------------------------------------------
+
+// String Zigzag
+
+// Have the function StringChallenge(strArr) read the array of string stored in strArr, which will contain the elements, the first some sort of string and the second element will be a number ranging from 1 to 6. The number represent how many rows to print the string on so that it forms a zig-zag pattern. For example: if strArr is ["coderbyte", "3"] the this word will look like the following if you print it in a zig-zag pattern with 3 rows:
+
+// c      r     e
+//   o  e  b  t
+//    d     y
+
+// Your program should return the word form by combining the characters as you iterate through each row, so for this example your program should return the string creoebtdy.
+
+// Examples:
+
+// Input: ['cat', '5']
+// Output: cat
+
+// Input: ['kaamvjjfl', '4']
+// Output: kjajfavlm
+
+// function StringChallenge(strArr) {
+//     const string = strArr[0];
+//     const range = Number(strArr[1]);
+
+//     if (range === 1) {
+//         return string;
+//     }
+
+//     const rows = Array.from({ length: range }, () => []);
+
+//     let row = 0;
+//     let direction = 1;
+
+//     for (let i = 0; i < string.length; i++) {
+//         const char = string[i];
+//         rows[row].push(char);
+
+//         const isToChangeDirection = row === range - 1; // -- reached last row
+
+//         if (row === 0) direction = 1;
+//         if (isToChangeDirection) direction = -1;
+
+//         row += direction;
+//     }
+
+//     return rows.flat().join('');
+// }
+
+// console.log(StringChallenge(['cat', '5'])); //=> cat
+// console.log(StringChallenge(['kaamvjjfl', '4'])); //=> kjajfavlm
+// console.log(StringChallenge(['coderbyte', '3'])); //=> creoebtdy
+
+//------------------------------------------------------------------------------------
+
+// Food Distribution
+
+// Have a function ArrayChallenge(arr) read the array of numbers stored in arr which will represent the hunger level of different people ranging from 0 to 5 (0 meaning not hungry at all, 5 meaning very hungry). You would also have N sandwiches to give out which will range from 1 to 20. The format of the array will be [N, h1, h2, h3,...] When N represents the number of sandwiches you have and the rest of the array will represent the hunger level of different people. Your goal is to minimize the hunger difference between each pair of people in the array using the sandwiches you have available.
+
+// For example: if arr is [5, 3, 1, 2, 1], this mean you have five sandwiches to give out. You can distribute them in the following order to the people: 2, 0, 1, 0. Giving these sandwiches to the people their hunger levels now become: [1, 1, 1, 1]. The difference between each pair of people is now 0, the total is also 0, so your program should return 0. Note: you may have to give out all, or even any, of your sandwiches to produce a minimize difference.
+
+// Another example: if arr is [4, 5, 2, 3, 1, 0] then you can distribute the sandwiches in the following order: [3, 0, 1, 0, 0] which makes all of the hunger levels the following: [2, 2, 2, 1, 0]. The difference between each pair of people is now: 0, 0, 1, 1 and so your program should return the final minimized difference of 2.
+
+// Examples:
+
+// Input: [5, 2, 3, 4, 5]
+// Output: 1
+
+// Input: [3, 2, 1, 0, 4, 1, 0]
+// Output: 4
+
+// function ArrayChallenge(arr) {
+//     let N = arr.shift();
+//     let hungers = arr;
+//     let diffs = differences(hungers);
+//     if (N >= diffs) {
+//         return 0;
+//     }
+//     while (N > 0 && sum(diffs) > 0) {
+//         let combos = [];
+//         for (let i = 0; i < hungers.length; i++) {
+//             let combo = hungers.slice();
+//             combo[i]--;
+//             combos.push(combo);
+//         }
+
+//         hungers = combos.reduce(minDiff);
+//         N--;
+
+//         diffs = differences(hungers);
+//     }
+
+//     return sum(diffs);
+// }
+
+// // HELPER METHODS
+// // Returns an array of differences across each pair
+// function differences(array) {
+//     let diffs = [];
+
+//     for (let i = 0; i < array.length - 1; i++) {
+//         diffs.push(Math.abs(array[i] - array[i + 1]));
+//     }
+//     return diffs;
+// }
+
+// // Returns the sum of all values in an array (i.e. sum of all diffs)
+// function sum(array) {
+//     return array.reduce((p, c) => p + c, 0);
+// }
+
+// // Compares two array and returns the array with the smallest sum of differences
+// function minDiff(arr1, arr2) {
+//     if (sum(differences(arr1)) <= sum(differences(arr2))) {
+//         return arr1;
+//     } else {
+//         return arr2;
+//     }
+// }
+
+// console.log(ArrayChallenge([5, 2, 3, 4, 5])); //=> 1
+// console.log(ArrayChallenge([3, 2, 1, 0, 4, 1, 0])); //=> 4
+
+// Another Answer
+
+// var totalDiff = function (arr) {
+//     var sum = 0;
+//     for (var i = 0; i < arr.length - 1; i++) {
+//         sum += Math.abs(arr[i] - arr[i + 1]);
+//     }
+//     return sum;
+// };
+
+// var ArrayChallenge = function (arr) {
+//     var food = arr.shift();
+//     var diff = totalDiff(arr);
+//     var potentialDiff = diff;
+
+//     while (food > 0) {
+//         var lowestI;
+//         for (var i = 0; i < arr.length; i++) {
+//             var newArr = arr.slice(0);
+//             newArr[i]--;
+//             if (totalDiff(newArr) <= potentialDiff) {
+//                 lowestI = i;
+//                 potentialDiff = totalDiff(newArr);
+//             }
+//         }
+//         if (potentialDiff <= diff) {
+//             arr[lowestI]--;
+//             diff = potentialDiff;
+//             food--;
+//         } else {
+//             return diff;
+//         }
+//     }
+
+//     return diff;
+// };
+
+// console.log(ArrayChallenge([5, 2, 3, 4, 5])); //=> 1
+// console.log(ArrayChallenge([3, 2, 1, 0, 4, 1, 0])); //=> 4
+
+//------------------------------------------------------------------------------------
+
+// Searching Challenge
+
+// Have the function SearchingChallenge(strArr) take the array of strings stored in strArr, which will be 2D matrix of 0 and 1's, and determine how many holes, or contiguous regions of 0's, exist in the matrix. A contiguous region is one where there is a connected group of 0's going in one or more of four directions: up, down, left, or right. For example: if strArr is ["10111", "10101", "11101", "11111"], then this looks like the following matrix:
+
+// 1 0 1 1 1
+// 1 0 1 0 1
+// 1 1 1 0 1
+// 1 1 1 1 1
+
+// For the input above, your program should return 2 because there are two separate contiguous regions of 0's. which create "holes" in the matrix. You can assume the input will not be empty.
+
+// Examples:
+
+// Input: ["01111", "01101", "00011", "11110"]
+// Output: 3
+
+// Input: ["1011", "0010"]
+// Output: 2
+
+// function SearchingChallenge(strArr) {
+//     let ret = [];
+//     let indexs = [];
+//     let subStr = [];
+//     for (const element of strArr) {
+//         subStr = element.split('');
+//         let index = [];
+//         for (let y = 0; y < subStr.length; y++) {
+//             if (subStr[y] == 0) {
+//                 index.push(y);
+//             }
+//             if (y == subStr.length - 1) {
+//                 indexs.push(index);
+//             }
+//         }
+//     }
+//     for (let i = 0; i < indexs.length; i++) {
+//         for (let j = 0; j < indexs[i].length; j++) {
+//             if (
+//                 indexs[i + 1] &&
+//                 (indexs[i][j] === indexs[i + 1][j] ||
+//                     indexs[i + 1].indexOf(indexs[i][j]))
+//             ) {
+//                 if (ret.indexOf(strArr[i]) === -1) {
+//                     ret.push(strArr[i]);
+//                 }
+//             }
+
+//             if (Math.abs(indexs[i][j] - indexs[i][j + 1]) === 1) {
+//                 if (ret.indexOf(strArr[i]) === -1) {
+//                     ret.push(strArr[i]);
+//                 }
+//             }
+//         }
+//     }
+
+//     return ret.length;
+// }
+
+// // keep this function call here
+// console.log(SearchingChallenge(['1011', '0010'])); //=> 2
+// console.log(SearchingChallenge(['01111', '01101', '00011', '11110'])); //=> 3
+
+//-------------------------------------------------------------------------------
